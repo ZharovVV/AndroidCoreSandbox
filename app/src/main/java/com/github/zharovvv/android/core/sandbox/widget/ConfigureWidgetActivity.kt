1c156bccc1aa98930166447bcfe57c2311ce5fd2
Vitaly Zharov <zharov.vitalik@gmail.com>
2021-10-31 21:48:29 +0500

    Widget (2 часть). Добавлениие активити для конфигурирования виджета.

---
+++ app/src/main/java/com/github/zharovvv/android/core/sandbox/widget/ConfigureWidgetActivity.kt
@@ -0,0 +1,87 @@
+package com.github.zharovvv.android.core.sandbox.widget
+
+import android.appwidget.AppWidgetManager
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.github.zharovvv.android.core.sandbox.R
+
+/**
+ * Данное Activity будет запускаться системой при добавлении нового экземпляра виджета
+ * и на вход получать ID этого экземпляра.
+ */
+class ConfigureWidgetActivity : AppCompatActivity() {
+
+    companion object {
+        private const val LOG_TAG = "WIDGET"
+        const val WIDGET_PREF = "widget_pref"
+        const val WIDGET_TEXT = "widget_text_"
+        const val WIDGET_COLOR = "widget_color_"
+    }
+
+    var widgetID = AppWidgetManager.INVALID_APPWIDGET_ID
+    var resultValue: Intent? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i(LOG_TAG, "onCreate config")
+
+        // извлекаем ID конфигурируемого виджета
+        val intent = intent
+        val extras = intent.extras
+        if (extras != null) {
+            widgetID = extras.getInt(
+                AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID
+            )
+        }
+        // и проверяем его корректность
+        if (widgetID == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish()
+        }
+
+        // формируем intent ответа
+        resultValue = Intent()
+        resultValue!!.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID)
+
+        // отрицательный ответ
+        // Хелп рекомендует при создании Activity сразу формировать отрицательный результат.
+        // В этом случае, если пользователь нажмет Назад, система получит ответ,
+        // что виджет создавать не надо.
+        setResult(RESULT_CANCELED, resultValue)
+        setContentView(R.layout.activity_configure_widget)
+    }
+
+
+    fun onClick(v: View?) {
+        val selRBColor = (findViewById<View>(R.id.rgColor) as RadioGroup)
+            .checkedRadioButtonId
+        var color: Int = Color.RED
+        when (selRBColor) {
+            R.id.radioRed -> color = Color.parseColor("#66ff0000")
+            R.id.radioGreen -> color = Color.parseColor("#6600ff00")
+            R.id.radioBlue -> color = Color.parseColor("#660000ff")
+        }
+        val etText = findViewById<EditText>(R.id.etText)
+
+        // Записываем значения с экрана в Preferences
+        val sp = getSharedPreferences(WIDGET_PREF, MODE_PRIVATE)
+        sp.edit()
+            .apply {
+                putString(WIDGET_TEXT + widgetID, etText.text.toString())
+                putInt(WIDGET_COLOR + widgetID, color)
+            }.apply()
+        //фикс бага с вызовом AppWidgetProvider#onUpdate перед вызовом ConfigureWidgetActivity:
+        val appWidgetManager = AppWidgetManager.getInstance(this)
+        ExampleAppWidgetProvider.updateWidget(widgetID, this, appWidgetManager, sp)
+        // положительный ответ
+        setResult(RESULT_OK, resultValue)
+        Log.i(LOG_TAG, "finish config $widgetID")
+        finish()
+    }
+}
--- app/src/main/java/com/github/zharovvv/android/core/sandbox/widget/ExampleAppWidgetProvider.kt
+++ app/src/main/java/com/github/zharovvv/android/core/sandbox/widget/ExampleAppWidgetProvider.kt
@@ -3,7 +3,12 @@ package com.github.zharovvv.android.core.sandbox.widget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import android.util.Log
+import android.widget.RemoteViews
+import androidx.core.content.edit
+import com.github.zharovvv.android.core.sandbox.R
 
 /**
  * [AppWidgetProvider] - класс-наследник [android.content.BroadcastReceiver].
@@ -26,6 +31,37 @@ class ExampleAppWidgetProvider : AppWidgetProvider() {
 
     companion object {
         private const val LOG_TAG = "WIDGET"
+
+        /**
+         * За отображение виджета отвечает один процесс (какой-нибудь Home),
+         * а код из [ExampleAppWidgetProvider] будет выполняться в другом, своем собственном процессе.
+         * Поэтому у нас нет прямого доступа к view-компонентам виджета.
+         * И мы не можем вызывать метода типа setText и setBackgroundColor напрямую.
+         * Поэтому используется класс [RemoteViews], он предназначен для межпроцессной работы с view.
+         *
+         * Помним, что ОС Android - это многопользовательская система Linux,
+         * в которой каждое приложение это отдельный пользователь.
+         * По умолчанию каждое приложение работает в собственном процессе Linux.
+         * У каждого процесса есть собственная виртуальная машина,
+         * поэтому код приложения работает изолированно от других приложений.
+         */
+        fun updateWidget(
+            widgetId: Int,
+            context: Context?,
+            appWidgetManager: AppWidgetManager?,
+            sp: SharedPreferences?
+        ) {
+            // Читаем параметры из Preferences
+            val widgetText: String =
+                sp?.getString(ConfigureWidgetActivity.WIDGET_TEXT + widgetId, null) ?: return
+            val widgetColor: Int = sp.getInt(ConfigureWidgetActivity.WIDGET_COLOR + widgetId, 0)
+            // Настраиваем внешний вид виджета
+            val widgetView: RemoteViews = RemoteViews(context?.packageName, R.layout.widget)
+            widgetView.setTextViewText(R.id.tv, widgetText)
+            widgetColor.let { widgetView.setInt(R.id.tv, "setBackgroundColor", it) }
+            // Обновляем виджет
+            appWidgetManager?.updateAppWidget(widgetId, widgetView)
+        }
     }
 
     /**
@@ -43,6 +79,21 @@ class ExampleAppWidgetProvider : AppWidgetProvider() {
      * и список ID экземпляров виджетов [appWidgetIds], которые обновляются.
      * Именно этот метод обычно содержит код, который обновляет содержимое виджета.
      * Для этого нам нужен будет [AppWidgetManager], который мы получаем на вход.
+     *
+     * -----------------------------------
+     *
+     * onUpdate вызвался ПЕРЕД конфигурированием (когда настройки виджета еще не были сохранены),
+     * но не вызвался ПОСЛЕ завершения работы конфигурационного экрана
+     * (когда настройки сохранены и их можно использовать для обновления).
+     * Т.е. настройки мы в Activity сохранили, но виджет их пока не прочел.
+     * Теперь эти настройки применятся только при следующем обновлении,
+     * т.е. через 40 минут, как мы указывали в файле метаданных.
+     * Зачем делается onUpdate перед вызовом Activity – непонятно.
+     * К тому же хелп английским по белому цинично и жирным шрифтом заявляет, что не может такого быть:
+     * "… when you implement the Activity: … The onUpdate() method will not be called when the App Widget is created."
+     * В общем, это известный баг, смиримся с ним.
+     * К тому же у нас в методе updateWidget стоит защита от этого – мы проверяем,
+     * что в Preferences записан WIDGET_TEXT. Если его нет, то мы ничего не делаем.
      */
     override fun onUpdate(
         context: Context?,
@@ -50,7 +101,16 @@ class ExampleAppWidgetProvider : AppWidgetProvider() {
         appWidgetIds: IntArray?
     ) {
         super.onUpdate(context, appWidgetManager, appWidgetIds)
-        Log.i(LOG_TAG, "ExampleAppWidgetProvider#onUpdate")
+        Log.i(
+            LOG_TAG,
+            "ExampleAppWidgetProvider#onUpdate; appWidgetIds: ${
+                appWidgetIds?.joinToString(separator = ",") { it.toString() }
+            }}"
+        )
+        val sp = context?.getSharedPreferences(ConfigureWidgetActivity.WIDGET_PREF, MODE_PRIVATE)
+        for (id in appWidgetIds!!) {
+            updateWidget(id, context, appWidgetManager, sp)
+        }
     }
 
     /**
@@ -59,7 +119,16 @@ class ExampleAppWidgetProvider : AppWidgetProvider() {
      */
     override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
         super.onDeleted(context, appWidgetIds)
-        Log.i(LOG_TAG, "ExampleAppWidgetProvider#onDeleted")
+        Log.i(LOG_TAG, "ExampleAppWidgetProvider#onDeleted; appWidgetIds: ${
+            appWidgetIds?.joinToString(separator = ",") { it.toString() }
+        }")
+        val sp = context?.getSharedPreferences(ConfigureWidgetActivity.WIDGET_PREF, MODE_PRIVATE)
+        sp?.edit {
+            appWidgetIds?.forEach { widgetId: Int ->
+                remove(ConfigureWidgetActivity.WIDGET_TEXT + widgetId)
+                remove(ConfigureWidgetActivity.WIDGET_COLOR + widgetId)
+            }
+        }
     }
 
     /**
