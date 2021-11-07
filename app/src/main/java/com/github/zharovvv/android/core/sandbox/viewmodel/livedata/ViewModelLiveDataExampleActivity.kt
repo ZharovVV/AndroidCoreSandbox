17d7bb1212e1f06c8d4c2bc41f7844406d069688
Vitaly Zharov <zharov.vitalik@gmail.com>
2021-11-07 21:18:39 +0500

    Заметки о LifecycleOwner и Lifecycle.

--- app/src/main/java/com/github/zharovvv/android/core/sandbox/viewmodel/livedata/ViewModelLiveDataExampleActivity.kt
+++ app/src/main/java/com/github/zharovvv/android/core/sandbox/viewmodel/livedata/ViewModelLiveDataExampleActivity.kt
@@ -1,14 +1,20 @@
 package com.github.zharovvv.android.core.sandbox.viewmodel.livedata
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.lifecycle.*
 import com.github.zharovvv.android.core.sandbox.LogLifecycleAppCompatActivity
 import com.github.zharovvv.android.core.sandbox.R
 
 class ViewModelLiveDataExampleActivity :
     LogLifecycleAppCompatActivity(R.layout.activity_view_model_livedata_example) {
+
+    companion object {
+        private const val LOG_TAG = "LifecycleComponent"
+    }
 
     private lateinit var startButton: Button
     private lateinit var textView: TextView
@@ -37,8 +43,33 @@ class ViewModelLiveDataExampleActivity :
         // Второй параметр - это непосредственно подписчик, т.е. колбэк,
         // в который LiveData будет отправлять данные.
         // В нем только один метод onChanged. В нашем примере туда будет приходить String.
-        liveDataBasedViewModel.data.observe(this) { data: String ->
+        liveDataBasedViewModel.data.observe(this, Observer { data: String ->
             textView.text = data
-        }
+        })
+
+        lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(value = Lifecycle.Event.ON_RESUME)
+            fun someFun() {
+                Log.i(LOG_TAG, "LifecycleObserver#someFun")
+            }
+        })
+        // При добавлении LifecycleEventObserver-а к списку наблюдающих — наблюдатель получит события
+        // об изменении всех состояний, которые предшествуют текущему.
+        // То есть если LifecycleOwner находится в состоянии Lifecycle.State.STARTED
+        // при добавлении LifecycleObserver,
+        // то последний получит два события Lifecycle.Event.ON_CREATE и Lifecycle.Event.ON_START.
+        //
+        // Значит, мы имеем гарантию того,
+        // что наш наблюдатель пройдет все ступени инициализации,
+        // опираясь на события жизненного цикла, и не пропустит какой-либо стадии конфигурации.
+        lifecycle.addObserver(object : LifecycleEventObserver {
+            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                val sourceCurrentState: Lifecycle.State = source.lifecycle.currentState
+                Log.i(
+                    LOG_TAG,
+                    "LifecycleEventObserver#onStateChanged; sourceCurrentState: $sourceCurrentState; event: $event;"
+                )
+            }
+        })
     }
 }
