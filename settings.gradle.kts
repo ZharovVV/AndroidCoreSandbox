72a4fe50ea41995c66fe6a5b6dd190e011d2cdf0
Zharov Vitaly <zharov.vitalik@gmail.com>
2023-01-17 01:07:42 +0500

    Up gradle version (ч.2).

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,15 +17,15 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("kotlin", "1.7.0") //такая версия из-за Compose
-            // Версия Компилятора Compose 1.2.0 совместима c kotlin 1.7.0
+            // Версия Компилятора Compose 1.2.0 совместимая c kotlin 1.7.0
 
             //android
-            alias("androidx-core-ktx").to("androidx.core:core-ktx:1.8.0")
-            alias("androidx-appcompat").to("androidx.appcompat:appcompat:1.4.2")
-            alias("androidx-constraintlayout").to("androidx.constraintlayout:constraintlayout:2.1.3")
-            alias("material").to("com.google.android.material:material:1.5.0")
+            library("androidx-core-ktx", "androidx.core:core-ktx:1.8.0")
+            library("androidx-appcompat", "androidx.appcompat:appcompat:1.4.2")
+            library("androidx-constraintlayout", "androidx.constraintlayout:constraintlayout:2.1.3")
+            library("material", "com.google.android.material:material:1.5.0")
             //for delegating ViewModel (by viewModels())
-            alias("androidx-activity-ktx").to("androidx.activity:activity-ktx:1.5.0")
+            library("androidx-activity-ktx", "androidx.activity:activity-ktx:1.5.0")
             bundle(
                 "core",
                 listOf(
@@ -38,44 +38,56 @@ dependencyResolutionManagement {
             )
             //jetpack
             //aac
-            alias("jetpack-workmanager").to("androidx.work:work-runtime:2.7.1")
-            alias("jetpack-workmanager-ktx").to("androidx.work:work-runtime-ktx:2.7.1")
+            library("jetpack-workmanager", "androidx.work:work-runtime:2.7.1")
+            library("jetpack-workmanager-ktx", "androidx.work:work-runtime-ktx:2.7.1")
             bundle("jetpack-workmanager", listOf("jetpack-workmanager", "jetpack-workmanager-ktx"))
 
             //где-то тут тянется библиотека ktx Fragment с функциями-расширениями типа commit {..}
-            alias("jetpack-navigation-fragment").to("androidx.navigation:navigation-fragment-ktx:2.4.2")
-            alias("jetpack-navigation-ui").to("androidx.navigation:navigation-ui-ktx:2.4.2")
+            library(
+                "jetpack-navigation-fragment",
+                "androidx.navigation:navigation-fragment-ktx:2.4.2"
+            )
+            library("jetpack-navigation-ui", "androidx.navigation:navigation-ui-ktx:2.4.2")
             bundle(
                 "jetpack-navigation",
                 listOf("jetpack-navigation-fragment", "jetpack-navigation-ui")
             )
             //compose
             version("compose", "1.2.0")
-            alias("jetpack-compose-ui").to("androidx.compose.ui", "ui").versionRef("compose")
+            library("jetpack-compose-ui", "androidx.compose.ui", "ui").versionRef("compose")
             // Tooling support (Previews, etc.)
-            alias("jetpack-compose-ui-tooling")
-                .to("androidx.compose.ui", "ui-tooling")
+            library("jetpack-compose-ui-tooling", "androidx.compose.ui", "ui-tooling")
                 .versionRef("compose")
             // Foundation (Border, Background, Box, Image, Scroll, shapes, animations, etc.)
-            alias("jetpack-compose-foundation")
-                .to("androidx.compose.foundation", "foundation")
+            library("jetpack-compose-foundation", "androidx.compose.foundation", "foundation")
                 .versionRef("compose")
             // Material Design
-            alias("jetpack-compose-material3")
-                .to("androidx.compose.material3:material3:1.0.0-alpha14")
-            alias("jetpack-compose-material-icons-core")
-                .to("androidx.compose.material", "material-icons-core")
+            library(
+                "jetpack-compose-material3",
+                "androidx.compose.material3:material3:1.0.0-alpha14"
+            )
+            library(
+                "jetpack-compose-material-icons-core",
+                "androidx.compose.material",
+                "material-icons-core"
+            )
                 .versionRef("compose")
-            alias("jetpack-compose-material-icons-extended")
-                .to("androidx.compose.material", "material-icons-extended")
+            library(
+                "jetpack-compose-material-icons-extended",
+                "androidx.compose.material",
+                "material-icons-extended"
+            )
                 .versionRef("compose")
             // Integration with activities
-            alias("jetpack-compose-activity").to("androidx.activity:activity-compose:1.4.0")
+            library("jetpack-compose-activity", "androidx.activity:activity-compose:1.4.0")
             // Integration with ViewModels
-            alias("jetpack-compose-viewmodel").to("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0")
+            library(
+                "jetpack-compose-viewmodel",
+                "androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0"
+            )
             // Integration with observables
-            alias("jetpack-compose-livedata").to("androidx.compose.runtime:runtime-livedata:1.1.1")
-            alias("jetpack-compose-rxjava2").to("androidx.compose.runtime:runtime-rxjava2:1.1.1")
+            library("jetpack-compose-livedata", "androidx.compose.runtime:runtime-livedata:1.1.1")
+            library("jetpack-compose-rxjava2", "androidx.compose.runtime:runtime-rxjava2:1.1.1")
             bundle(
                 "jetpack-compose",
                 listOf(
@@ -93,30 +105,30 @@ dependencyResolutionManagement {
             )
 
             //unit tests
-            alias("junit").to("junit:junit:4.13.2")
+            library("junit", "junit:junit:4.13.2")
             //maybe mockk
             bundle("test", listOf("junit"))
 
             //android tests
-            alias("android-junit").to("androidx.test.ext:junit:1.1.3")
-            alias("android-espresso").to("androidx.test.espresso:espresso-core:3.4.0")
+            library("android-junit", "androidx.test.ext:junit:1.1.3")
+            library("android-espresso", "androidx.test.espresso:espresso-core:3.4.0")
             bundle("androidTest", listOf("android-junit", "android-espresso"))
-            alias("androidTest-compose").to("androidx.compose.ui:ui-test-junit4:1.1.1")
+            library("androidTest-compose", "androidx.compose.ui:ui-test-junit4:1.1.1")
 
             //dagger
             version("dagger2", "2.42")
-            alias("dagger").to("com.google.dagger", "dagger").versionRef("dagger2")
-            alias("dagger-compiler").to("com.google.dagger", "dagger-compiler")
+            library("dagger", "com.google.dagger", "dagger").versionRef("dagger2")
+            library("dagger-compiler", "com.google.dagger", "dagger-compiler")
                 .versionRef("dagger2")
 
             //rxjava2
-            alias("rxjava2").to("io.reactivex.rxjava2:rxjava:2.2.9")
-            alias("rxjava2-android").to("io.reactivex.rxjava2:rxandroid:2.1.1")
-            alias("rxjava2-kotlin").to("io.reactivex.rxjava2:rxkotlin:2.3.0")
+            library("rxjava2", "io.reactivex.rxjava2:rxjava:2.2.9")
+            library("rxjava2-android", "io.reactivex.rxjava2:rxandroid:2.1.1")
+            library("rxjava2-kotlin", "io.reactivex.rxjava2:rxkotlin:2.3.0")
             bundle("rxjava", listOf("rxjava2", "rxjava2-android", "rxjava2-kotlin"))
 
             //coroutines
-            alias("coroutines").to("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
+            library("coroutines", "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.1")
             //транзитивно тянет kotlinx-coroutines-core-jvm
         }
     }
