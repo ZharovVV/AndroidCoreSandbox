d0a266427c1bc0391d8c50261a7e165fd9cec3d2
Zharov Vitaly <zharov.vitalik@gmail.com>
2023-02-23 17:06:07 +0500

    Добавление плагина для проверки версий.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,16 @@ buildscript {
     repositories {
         google()
         mavenCentral()
+        //for se.ascp.gradle:gradle-versions-filter:0.1.16
+        maven {
+            url = uri("https://plugins.gradle.org/m2/")
+        }
     }
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.1")
         val kotlinVersion = libs.versions.kotlin.get()
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("se.ascp.gradle:gradle-versions-filter:0.1.16")
     }
 }
 
@@ -17,6 +22,18 @@ allprojects {
         mavenCentral()
     }
 }
+
+/**
+ * TODO удалить, когда для versionCatalog заработают предложения обновления версий
+ * [bug](https://issuetracker.google.com/issues/226078451)
+ *
+ * См. [описание работы](https://github.com/janderssonse/gradle-versions-filter-plugin)
+ * Для запуска плагина нужно выполнить задачу:
+ * ```
+ * ./gradlew dependencyUpdates
+ * ```
+ */
+apply(plugin = "se.ascp.gradle.gradle-versions-filter")
 
 subprojects {
     project.plugins.applyBaseAndroidConfig(project)
