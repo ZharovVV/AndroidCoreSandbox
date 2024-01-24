973db78e4554c494b44ca6e637ca2b1e5d458d21
Zharov Vitaly <zharov.vitalik@gmail.com>
2024-01-25 00:57:53 +0500

    Добавление анимации для CustomView.

--- core-ui/src/main/java/com/github/zharovvv/core/ui/custom/ClickableCircleView.kt
+++ core-ui/src/main/java/com/github/zharovvv/core/ui/custom/ClickableCircleView.kt
@@ -1,10 +1,15 @@
 package com.github.zharovvv.core.ui.custom
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.os.Build
 import android.util.AttributeSet
+import android.util.TypedValue
+import android.view.GestureDetector
+import android.view.GestureDetector.SimpleOnGestureListener
+import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.res.getColorOrThrow
 import androidx.core.content.res.getDimensionOrThrow
@@ -17,9 +22,22 @@ class ClickableCircleView @JvmOverloads constructor(
     defStyleRes: Int = 0
 ) : View(context, attrs, defStyleAttr, defStyleRes) {
 
-    var radius: Float
+    private var radius: Float
     private val paint: Paint = Paint()
         .apply { style = Paint.Style.FILL }
+
+    private val gestureDetector: GestureDetector =
+        GestureDetector(context, object : SimpleOnGestureListener() {
+            override fun onDown(e: MotionEvent): Boolean {
+                //Обязательно нужно для перехвата onDoubleTap
+                return true
+            }
+
+            override fun onDoubleTap(e: MotionEvent): Boolean {
+                onDoubleTap()
+                return true
+            }
+        })
 
     init {
         context.obtainStyledAttributes(
@@ -42,7 +60,18 @@ class ClickableCircleView @JvmOverloads constructor(
             paint.color = typedArray.getColorOrThrow(R.styleable.ClickableCircleView_circleColor)
             radius = typedArray.getDimensionOrThrow(R.styleable.ClickableCircleView_circleRadius)
         }
+        setOnTouchListener { _, motionEvent -> gestureDetector.onTouchEvent(motionEvent) }
     }
+
+    private val radiusAnimator: ValueAnimator =
+        ValueAnimator.ofFloat(radius, radius + 5.dp(), radius)
+            .apply {
+                addUpdateListener { animator ->
+                    radius = animator.animatedValue as Float
+                    invalidate()
+                }
+                duration = 300
+            }
 
 
     override fun onDraw(canvas: Canvas) {
@@ -50,6 +79,24 @@ class ClickableCircleView @JvmOverloads constructor(
         super.onDraw(canvas)
         canvas.drawCircle(width / 2f, height / 2f, radius, paint)
     }
+
+    private fun onDoubleTap() {
+        if (radiusAnimator.isStarted.not()) {
+            radiusAnimator.start()
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        radiusAnimator.cancel()
+    }
+
+    private fun Number.dp(): Float = TypedValue.applyDimension(
+        TypedValue.COMPLEX_UNIT_DIP,
+        toFloat(),
+        resources.displayMetrics
+    )
+
 
     private companion object {
         const val TAG = "ClickableCircleView"
